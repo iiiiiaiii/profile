8d9d5b2621772a5c5b54cbaa2c37af5b7773be9a
jang young kyu <qto1@naver.com>
2024-02-13 15:53:55 +0900

    first_commit

---
+++ src/main/java/MI/hello/HelloCompanyApplication.java
@@ -0,0 +1,13 @@
+package MI.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MI/hello/HelloCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package MI.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
